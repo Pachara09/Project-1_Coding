3514f02b6a08663bfc51da5c072474c96228eb5b
Pachara09 <phetpachara002@gmail.com>
2024-12-04 16:06:25 +0700

    Add files via upload

---
+++ 1form_09Phet.py
@@ -0,0 +1,10 @@
+print("Welcom to Wedsite")
+
+print("\n\nกรอกข้อมูล :)")
+Nickname = input("ชื่อ")
+Number = input("เลขที่")
+Grade = input("ระดับชั้น")
+
+print("\nยินดีต้อนรับ", Nickname)
+print("\nเลขที่", Number)
+print("\nอยู่ระดับชั้น", Grade)
